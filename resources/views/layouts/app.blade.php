e129aa6b2cc5c7da1d0a479086ddfd05cb6c6ccb
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2026-01-28 12:16:41 +0100

    Initial cleanup code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,13 +45,6 @@
           href="{{ Vite::asset('resources/images/favicon-96x96.png') }}"
           sizes="96x96"
     />
-    <!-- Fonts -->
-    <link rel="preconnect"
-          href="https://fonts.bunny.net"
-    >
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
-          rel="stylesheet"
-    />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
