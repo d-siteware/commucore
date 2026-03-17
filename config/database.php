25e967eed595b3e7abbdb6235cd849d79a86738d
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2026-03-17 22:58:04 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -36,7 +36,9 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('INSTANCE_PATH')
+                ? env('INSTANCE_PATH') . '/database/database.sqlite'
+                : env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
