70690fc186a010a578cb04163106e8fd82041ce2
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2026-07-17 19:03:27 +0200

    Update console.php
    remove demoseeding 6hrs

--- routes/console.php
+++ routes/console.php
@@ -5,23 +5,20 @@ declare(strict_types=1);
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function (): void {
+Artisan::command('inspire', function (): void
+{
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command('gdpr:pseudonymize-members')->monthly();
-Schedule::command('gdpr:purge-event-subscriptions')->daily();
-Schedule::command('gdpr:purge-unsubscribed-mailing-list')->daily();
-
-if (config('app.is_demo')) {
-    Schedule::command('commucore:demoseeder --force')
-        ->everySixHours()
-        ->withoutOverlapping()
-        ->runInBackground();
-}
-
-Schedule::command('datev:clean-archives --days=30')->daily();
-
+})
+    ->purpose('Display an inspiring quote')
+    ->hourly();
+Schedule::command('gdpr:pseudonymize-members')
+    ->monthly();
+Schedule::command('gdpr:purge-event-subscriptions')
+    ->daily();
+Schedule::command('gdpr:purge-unsubscribed-mailing-list')
+    ->daily();
+Schedule::command('datev:clean-archives --days=30')
+    ->daily();
 Schedule::command('queue:check-health')
     ->everyFiveMinutes()
     ->withoutOverlapping()
